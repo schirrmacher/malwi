d64f4b049c854fe73ad2487546ad13bbc4bdda2d
Marvin Schirrmacher <schirma@amazon.com>
2025-05-11 08:31:56 +0200

    fix missing package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="malwi",
-    version="0.0.2",
+    version="0.0.3",
     author="Marvin Schirrmacher",
     author_email="m@schirrmacher.io",
     description="malwi - AI Python Malware Scanner",
