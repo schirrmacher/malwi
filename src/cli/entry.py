36ab8cd03624cc35a7ab1ee032c7e9b7723793d7
Marvin Schirrmacher <schirma@amazon.com>
2025-05-12 12:55:27 +0200

    fix malicious-only flag

--- src/cli/entry.py
+++ src/cli/entry.py
@@ -57,7 +57,7 @@ def main():
         "-f",
         choices=["json", "yaml", "table"],
         default="table",
-        help="Specify the output format: 'json' or 'table'.",
+        help="Specify the output format: 'json', 'yaml', or 'table'.",
     )
     parser.add_argument(
         "--save",
@@ -74,6 +74,7 @@ def main():
     )
     parser.add_argument(
         "--malicious-only",
+        "-mo",
         action="store_true",
         help="Only include malicious findings in the output.",
     )
@@ -85,24 +86,26 @@ def main():
         default=0.5,
         help="Specify the threshold for classifying nodes as malicious (default: 0.5).",
     )
-    parser.add_argument(
+
+    developer_group = parser.add_argument_group("Developer Options")
+    developer_group.add_argument(
         "--debug",
         "-d",
         action="store_true",
         help="Print the model input before prediction.",
     )
-    parser.add_argument(
+    developer_group.add_argument(
         "--tokenizer-path",
         "-t",
         metavar="PATH",
-        help="Specify the custom tokenizer path (directory or file).",
+        help="Specify the custom tokenizer directory.",
         default=None,
     )
-    parser.add_argument(
+    developer_group.add_argument(
         "--model-path",
         "-m",
         metavar="PATH",
-        help="Specify the custom model path (directory or file).",
+        help="Specify the custom model path directory.",
         default=None,
     )
 
@@ -159,7 +162,7 @@ def main():
             n.maliciousness = maliciousness
             if maliciousness > args.maliciousness_threshold:
                 malicious_nodes.append(n)
-            else:
+            elif not args.malicious_only:
                 benign_nodes.append(n)
         else:
             logging.error(
