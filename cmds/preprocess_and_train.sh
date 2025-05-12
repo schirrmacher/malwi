eea28be7d1e42c9cc54431fdff3b227a4cc08371
Marvin Schirrmacher <schirma@amazon.com>
2025-05-12 22:04:09 +0200

    fix train flag again

--- cmds/preprocess_and_train.sh
+++ cmds/preprocess_and_train.sh
@@ -1,4 +1,4 @@
 uv run python -m src.research.normalize_data -o benign
 uv run python -m src.research.normalize_data -o malicious
 uv run python -m src.research.filter_data -b benign.csv -m malicious.csv 
-uv run python -m src.research.train -b benign_processed.csv --m malicious_processed.csv --epochs 3
+uv run python -m src.research.train -b benign_processed.csv -m malicious_processed.csv --epochs 3
