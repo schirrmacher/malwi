9bddd53fe78c8b7474f5203c5378700cee584afa
Marvin Schirrmacher <schirma@amazon.com>
2025-05-21 11:36:56 +0200

    improve util scripts

--- cmds/preprocess_and_train.sh
+++ cmds/preprocess_and_train.sh
@@ -2,6 +2,7 @@ rm benign.csv
 rm malicious.csv
 rm malicious_processed.csv
 rm benign_processed.csv
+rm -rf malwi_models
 uv run python -m src.research.disassemble_python '.repo_cache/benign_repos' -f csv -s benign.csv
 uv run python -m src.research.disassemble_python '.repo_cache/malicious_repos' -f csv -s malicious.csv
 uv run python -m src.research.filter_data -b benign.csv -m malicious.csv
--- cmds/train.sh
+++ cmds/train.sh
@@ -1,1 +1,2 @@
+rm -rf malwi_models
 uv run python -m src.research.train -b benign_processed.csv -m malicious_processed.csv --epochs 3 --num-proc 1
