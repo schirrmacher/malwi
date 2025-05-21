34032cf2d1f156d87533d77b8a16830338422182
Marvin Schirrmacher <schirma@amazon.com>
2025-05-21 08:08:34 +0200

    delete temp files before processing

--- cmds/preprocess_and_train.sh
+++ cmds/preprocess_and_train.sh
@@ -1,3 +1,7 @@
+rm benign.csv
+rm malicious.csv
+rm malicious_processed.csv
+rm benign_processed.csv
 uv run python -m src.research.disassemble_python '.repo_cache/benign_repos' -f csv -s benign.csv
 uv run python -m src.research.disassemble_python '.repo_cache/malicious_repos' -f csv -s malicious.csv
 uv run python -m src.research.filter_data -b benign.csv -m malicious.csv
