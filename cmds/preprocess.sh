c5675a759fef109a2b185e01906a842cba3c1eb3
Marvin Schirrmacher <schirma@amazon.com>
2025-06-01 13:27:24 +0200

    adapt commands

--- cmds/preprocess.sh
+++ cmds/preprocess.sh
@@ -4,4 +4,4 @@ rm malicious_processed.csv
 rm benign_processed.csv
 uv run python -m src.research.disassemble_python '.repo_cache/benign_repos' -f csv -s benign.csv
 uv run python -m src.research.disassemble_python '.repo_cache/malicious_repos' -f csv -s malicious.csv
-uv run python -m src.research.filter_data -b benign.csv -m malicious.csv
+uv run python -m src.research.filter_data -b benign.csv -m malicious.csv --triaging triaging
