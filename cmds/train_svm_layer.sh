281eba177866403578572a88fd9a5c3b3f99fc87
Marvin Schirrmacher <schirma@amazon.com>
2025-06-10 21:28:02 +0200

    change training util script

---
+++ cmds/train_svm_layer.sh
@@ -0,0 +1,3 @@
+uv run python -m src.research.create_svm_data --label benign --dir .repo_cache/benign_repos  -s benign_svm.csv --benign-samples 10000 --max-files-per-sample 100
+uv run python -m src.research.create_svm_data --label malicious --dir .repo_cache/malicious_repos/pypi_malregistry  -s malicious_svm.csv
+uv run python -m src.research.train_svm_layer -b benign_svm.csv -m malicious_svm.csv
