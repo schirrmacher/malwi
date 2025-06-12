e0e7586d05c7891dab158576e679879a708c7ca5
Marvin Schirrmacher <schirma@amazon.com>
2025-06-12 10:18:57 +0200

    adapt scripts to point to local samples

--- cmds/train_svm_layer.sh
+++ cmds/train_svm_layer.sh
@@ -1,3 +1,5 @@
+rm benign_svm.csv
+rm malicious_svm.csv
 uv run python -m src.research.create_svm_data --label benign --dir .repo_cache/benign_repos  -s benign_svm.csv --benign-samples 10000 --max-files-per-sample 100
-uv run python -m src.research.create_svm_data --label malicious --dir .repo_cache/malicious_repos/pypi_malregistry  -s malicious_svm.csv
+uv run python -m src.research.create_svm_data --label malicious --dir samples/malicious  -s malicious_svm.csv
 uv run python -m src.research.train_svm_layer -b benign_svm.csv -m malicious_svm.csv
