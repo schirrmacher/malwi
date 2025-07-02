596b9160dff963f362aeb31cceca52944d37ef98
Marvin Schirrmacher <ms@schirrmacher.io>
2025-07-02 21:55:08 +0200

    enforce python >=3.12

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
+    python_requires=">=3.12",
     package_data={
         "research.syntax_mapping": [
             "compression_mapping.json",
